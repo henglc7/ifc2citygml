f78c93aa3200571c9588d27157120680221ffe71
fredy.bello <fredy.bello@gmail.com>
2014-02-27 00:00:16 +0000

    Se implementa cortado de muros con planos

--- IFC2CityGMLV2/src/org/udistrital/ifc2citygmlv2/sbm/Poligono.java
+++ IFC2CityGMLV2/src/org/udistrital/ifc2citygmlv2/sbm/Poligono.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.math3.geometry.euclidean.threed.Line;
+import org.apache.commons.math3.geometry.euclidean.threed.Plane;
+import org.apache.commons.math3.geometry.euclidean.threed.PolyhedronsSet;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.geometry.partitioning.Region;
+
 public class Poligono {
 	
 	private List<Coordenada> coordenadas;
@@ -44,6 +50,175 @@ public class Poligono {
 			
 		return cadena;
 		
+	}
+	
+	public Poligono cortar(Poligono caraActual, Plane planoEvaluado, PolyhedronsSet cajaFrontera/*, Poligono caraSuperior*/){
+		
+		
+		Vector3D normal = planoEvaluado.getNormal();
+		
+		Poligono caraCortada = new Poligono();
+		
+		Iterator<Coordenada> iA = caraActual.getCoordenadas().iterator();
+		Iterator<Coordenada> iB = caraActual.getCoordenadas().iterator();
+		
+		iB.next();
+		
+		PolyhedronsSet planoComoSolido = planoEvaluado.wholeSpace();
+		
+		boolean agregarA = true;
+		
+		System.err.println("ORIGEN = " + new Coordenada(planoEvaluado.getOrigin()) );
+		
+		
+		
+		while(iB.hasNext()) {
+			
+			Coordenada coordenadaA = iA.next();
+			Coordenada coordenadaB = iB.next();
+			
+			
+			double posicionA = planoEvaluado.getOffset(coordenadaA.toVector3D());
+			double posicionB = planoEvaluado.getOffset(coordenadaB.toVector3D());
+			
+		
+			
+			Line lineaDeAHastaB = new Line(coordenadaA.toVector3D(), coordenadaB.toVector3D());
+			
+			Vector3D interseccion = planoEvaluado.intersection(lineaDeAHastaB);
+			
+			/*
+			Boolean interseccionDentroDeCajaFrontera = null;
+			
+			if( interseccion != null){
+				if(cajaFrontera.checkPoint(interseccion)!= Region.Location.OUTSIDE){
+					//la interseccion de la linea con el plano esta DENTRO de la caja frontera del solido
+					interseccionDentroDeCajaFrontera = true;
+				}else{
+					//la interseccion de la linea con el plano esta FUERA de la caja frontera del solido
+					interseccionDentroDeCajaFrontera = false;
+				}
+			}else{
+				//la interseccion de la linea con el plano es INEXISTENTE porque son paralelos
+			}
+			
+			if(interseccionDentroDeCajaFrontera!= null && interseccionDentroDeCajaFrontera){
+				
+				Coordenada i = new Coordenada(interseccion);
+				
+				if(!caraSuperior.contieneCoordenada(i)) caraSuperior.getCoordenadas().add(i);
+			}
+			*/
+			
+			
+			//A y B debajo del plano
+			if(posicionA < 0 && posicionB < 0){
+				caraCortada.coordenadas.add(coordenadaA);
+				caraCortada.coordenadas.add(coordenadaB);
+			}
+			
+			//A debajo y B en el plano
+			if(posicionA < 0 && posicionB == 0){
+				caraCortada.coordenadas.add(coordenadaA);
+				caraCortada.coordenadas.add(coordenadaB);
+				
+				//if(!caraSuperior.contieneCoordenada(coordenadaB)) caraSuperior.getCoordenadas().add(coordenadaB);
+			}
+			
+			//A debajo y B encima el plano
+			if(posicionA < 0 && posicionB > 0){
+				
+				Coordenada i = new Coordenada(interseccion);
+				
+				caraCortada.coordenadas.add(coordenadaA);
+				caraCortada.coordenadas.add(i);
+				
+				//if(!caraSuperior.contieneCoordenada(i)) caraSuperior.getCoordenadas().add(i);
+			}
+			
+			//A en y B encima el plano
+			if(posicionA == 0 && posicionB > 0){
+				
+				caraCortada.coordenadas.add(coordenadaA);
+				
+				//if(!caraSuperior.contieneCoordenada(coordenadaA)) caraSuperior.getCoordenadas().add(coordenadaA);
+
+			}
+			
+			//A encima y B encima del plano
+			if(posicionA > 0 && posicionB > 0){
+
+				//nada
+
+			}
+			
+			//A y B en el plano
+			if(posicionA == 0 && posicionB == 0){
+
+				caraCortada.coordenadas.add(coordenadaA);
+				caraCortada.coordenadas.add(coordenadaB);
+				
+				//if(!caraSuperior.contieneCoordenada(coordenadaA)) caraSuperior.getCoordenadas().add(coordenadaA);
+				//if(!caraSuperior.contieneCoordenada(coordenadaB)) caraSuperior.getCoordenadas().add(coordenadaB);
+				
+
+			}
+			
+			
+			//ahora para B primero y A despues
+
+
+			
+			//B debajo y A en el plano
+			if(posicionB < 0 && posicionA == 0){
+				caraCortada.coordenadas.add(coordenadaA);
+				caraCortada.coordenadas.add(coordenadaB);
+				
+				//if(!caraSuperior.contieneCoordenada(coordenadaA)) caraSuperior.getCoordenadas().add(coordenadaA);
+			}
+			
+			//B debajo y A encima el plano
+			if(posicionB < 0 && posicionA > 0){
+				
+				Coordenada i = new Coordenada(interseccion);
+				
+				caraCortada.coordenadas.add(i);
+				caraCortada.coordenadas.add(coordenadaB);
+				
+				//if(!caraSuperior.contieneCoordenada(i)) caraSuperior.getCoordenadas().add(i);
+				
+			}
+			
+			//B en y A encima el plano
+			if(posicionA == 0 && posicionB > 0){
+				
+				caraCortada.coordenadas.add(coordenadaB);
+				
+				//if(!caraSuperior.contieneCoordenada(coordenadaB)) caraSuperior.getCoordenadas().add(coordenadaB);
+
+			}
+			
+					
+		}
+		
+		return caraCortada;
+		
+		
+	}
+	
+	public boolean contieneCoordenada(Coordenada c){
+		boolean r = false;
+		
+		for (Coordenada coordenadaActual : coordenadas) {
+			
+			if(coordenadaActual.esIgual(c)){
+				r = true;
+				break;
+			}
+			
+		}
+		
+		return r;
 	}
 
 }
